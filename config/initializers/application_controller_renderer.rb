84a561a93902e8db0934af480a7c4ec2c662af05
yuma0222 <nakamura_poison@icloud.com>
2020-06-20 16:35:45 +0900

    [Finish]5-7

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'b041736394a9b91c4c22dbda47df2989f2bc3c0bfff519a08e37fee16c49506a9b74880f2d5d4e9b26910b9c587893fe217426a33d63c081ac168866b0116c03'
